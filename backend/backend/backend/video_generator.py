ba6e2691b4c65e17c5568169b8ea9665daca9353
sanjidrahaman007-max <sanjidrahaman007@gmail.com>
2026-04-17 21:42:54 +0600

    Create video_generator.py

---
+++ backend/backend/backend/video_generator.py
@@ -0,0 +1,10 @@
+from moviepy.editor import ColorClip, AudioFileClip
+
+def create_video(audio):
+    clip = ColorClip(size=(720,1280), color=(0,0,0), duration=8)
+
+    audio_clip = AudioFileClip(audio)
+    clip = clip.set_audio(audio_clip)
+
+    clip.write_videofile("output.mp4", fps=24)
+    return "output.mp4"
