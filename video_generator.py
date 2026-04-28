c10d982c3125ba105d19be43dfea9b3dbebb6fd7
sanjidrahaman007-max <sanjidrahaman007@gmail.com>
2026-04-28 12:25:24 +0600

    Delete video_generator.py

--- video_generator.py
+++
@@ -1,9 +0,0 @@
-from moviepy.editor import *
-
-def create_video(audio):
-    clip = ColorClip(size=(720,1280), color=(0,0,0), duration=10)
-    audio_clip = AudioFileClip(audio)
-    clip = clip.set_audio(audio_clip)
-
-    clip.write_videofile("output.mp4", fps=24)
-    return "output.mp4"
